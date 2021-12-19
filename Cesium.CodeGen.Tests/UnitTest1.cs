997960e28316887f4a7af0f649e94de715d3d765
Friedrich von Never <friedrich@fornever.me>
2021-12-19 23:18:23 +0700

    CodeGen: support int functions

---
+++ Cesium.CodeGen.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using Cesium.Parser;
+using Mono.Cecil;
+using Mono.Cecil.Rocks;
+using Xunit;
+using Yoakke.C.Syntax;
+
+namespace Cesium.CodeGen.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void EmptyMainTest()
+    {
+        const string source = "int main() {}";
+        var translationUnit = new CParser(new CLexer(source)).ParseTranslationUnit().Ok.Value;
+        var assembly = Generator.GenerateAssembly(
+            translationUnit,
+            new AssemblyNameDefinition("test", new Version()),
+            ModuleKind.Console);
+
+        var moduleType = assembly.Modules.Single().GetType("<Module>");
+        var method = moduleType.GetMethods().Single();
+        Assert.Equal("main", method.Name);
+    }
+}
---
+++ Cesium.CodeGen/Generator.cs
@@ -0,0 +1,45 @@
+﻿using Cesium.Ast;
+using Mono.Cecil;
+
+namespace Cesium.CodeGen;
+
+public class Generator
+{
+    public static AssemblyDefinition GenerateAssembly(
+        TranslationUnit translationUnit,
+        AssemblyNameDefinition name,
+        ModuleKind kind)
+    {
+        var assembly = AssemblyDefinition.CreateAssembly(name, "Primary", kind);
+        var module = assembly.MainModule;
+        var moduleType = module.GetType("<Module>");
+
+        foreach (var declaration in translationUnit.Declarations)
+        {
+            var method = GenerateMethod(module, (FunctionDefinition)declaration);
+            moduleType.Methods.Add(method);
+        }
+
+        return assembly;
+    }
+
+    private static MethodDefinition GenerateMethod(ModuleDefinition module, FunctionDefinition definition)
+    {
+        var def = new MethodDefinition(
+            definition.Declarator.DirectDeclarator.Name,
+            MethodAttributes.Public | MethodAttributes.Static,
+            GetReturnType(module, definition));
+
+        return def;
+    }
+
+    private static TypeReference GetReturnType(ModuleDefinition module, FunctionDefinition definition)
+    {
+        var typeSpecifier = definition.Specifiers.OfType<TypeSpecifier>().Single();
+        return typeSpecifier.TypeName switch
+        {
+            "int" => module.TypeSystem.Int32,
+            var unknown => throw new Exception($"Unknown type specifier: {unknown}")
+        };
+    }
+}
