da8cdfe3787da75f620bd619b826c3b059546767
Friedrich von Never <friedrich@fornever.me>
2021-12-21 23:00:02 +0700

    Parser: expression statement support

--- Cesium.Ast/Statements.cs
+++ Cesium.Ast/Statements.cs
@@ -4,9 +4,14 @@ namespace Cesium.Ast;
 
 // 6.8 Statements and blocks
 public abstract record Statement : IBlockItem;
+
+// 6.8.2 Compound statement
 public record CompoundStatement(ImmutableArray<IBlockItem> Block) : Statement;
 
 public interface IBlockItem {}
+
+// 6.8.3 Expression and null statements
+public record ExpressionStatement(Expression? Expression) : Statement;
 
 // 6.8.6 Jump statements
 public record GoToStatement(string Identifier) : Statement;
