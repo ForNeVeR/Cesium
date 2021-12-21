0b1a3175be2733fe7523cf1c066d28070ab9e503
Kouji Matsui <k@kekyo.net>
2021-12-21 21:14:13 +0900

    Added assigning target framework feature.

--- Cesium.CodeGen/Generator.cs
+++ Cesium.CodeGen/Generator.cs
@@ -6,13 +6,47 @@ namespace Cesium.CodeGen;
 
 public class Generator
 {
+    public enum TargetFrameworks
+    {
+        mscorlib,
+        SystemRuntime,
+        netstandard,
+    }
+
+    public record TargetRuntimeIdentifier(
+        TargetFrameworks targetFramework,
+        Version version);
+
+    private static readonly TargetRuntimeIdentifier defaultTargetRuntime =
+        new(TargetFrameworks.netstandard, new Version(2, 1, 0, 0));
+
     public static AssemblyDefinition GenerateAssembly(
         TranslationUnit translationUnit,
         AssemblyNameDefinition name,
-        ModuleKind kind)
+        ModuleKind kind,
+        TargetRuntimeIdentifier? targetRuntime = default)
     {
         var assembly = AssemblyDefinition.CreateAssembly(name, "Primary", kind);
         var module = assembly.MainModule;
+
+        var tr = targetRuntime ?? defaultTargetRuntime;
+        var (assemblyName, publicKeyToken) =
+            tr.targetFramework switch
+            {
+                TargetFrameworks.mscorlib =>
+                    ("mscorlib", new byte[] { 0xb7, 0x7a, 0x5c, 0x56, 0x19, 0x34, 0xe0, 0x89 }),
+                TargetFrameworks.SystemRuntime =>
+                    ("System.Runtime", new byte[] { 0xb0, 0x3f, 0x5f, 0x7f, 0x11, 0xd5, 0x0a, 0x3a }),
+                _ =>
+                    ("netstandard", new byte[] { 0xb0, 0x3f, 0x5f, 0x7f, 0x11, 0xd5, 0x0a, 0x3a })
+            };
+        var corlibReference = new AssemblyNameReference(
+            assemblyName, tr.version)
+        {
+            PublicKeyToken = publicKeyToken
+        };
+        module.AssemblyReferences.Add(corlibReference);
+
         var moduleType = module.GetType("<Module>");
 
         foreach (var declaration in translationUnit.Declarations)
