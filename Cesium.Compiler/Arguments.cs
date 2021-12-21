c88e4a0f412516783ac7178653ee14236a145235
Friedrich von Never <friedrich@fornever.me>
2021-12-21 14:29:37 +0700

    Arguments: fix warnings

--- Cesium.Compiler/Arguments.cs
+++ Cesium.Compiler/Arguments.cs
@@ -1,12 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
 using CommandLine;
 
 namespace Cesium.Compiler;
 
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
 public class Arguments
 {
     [Value(index: 0)]
-    public string InputFilePath { get; init; }
+    public string? InputFilePath { get; init; }
 
     [Value(index: 1)]
-    public string OutputFilePath { get; init; }
+    public string? OutputFilePath { get; init; }
 }
