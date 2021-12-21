dc75c95461dcc1c212ce95814820accfc46df35d
Andrii Kurdiumov <kant2002@gmail.com>
2021-12-21 21:09:50 +0600

    Add test for C identifier rules

--- Cesium.Parser.Tests/LexerTests.cs
+++ Cesium.Parser.Tests/LexerTests.cs
@@ -33,4 +33,67 @@ public class LexerTests
             "CloseBrace: }"
         }, tokens);
     }
+
+    [Fact]
+    public void ParametersTest()
+    {
+        const string source = "int main(int argc, char  *argv [ ]) {}";
+        var tokens = GetTokens(source).Select(t => $"{t.Kind}: {t.Text}");
+        Assert.Equal(new[]
+        {
+            "KeywordInt: int",
+            "Identifier: main",
+            "OpenParen: (",
+            "KeywordInt: int",
+            "Identifier: argc",
+            "Comma: ,",
+            "KeywordChar: char",
+            "Multiply: *",
+            "Identifier: argv",
+            "OpenBracket: [",
+            "CloseBracket: ]",
+            "CloseParen: )",
+            "OpenBrace: {",
+            "CloseBrace: }"
+        }, tokens);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidIdentifiers))]
+    public void IncorrectIdentifierTest(string source)
+    {
+        var tokens = string.Join("||", GetTokens(source).Select(t => $"{t.Kind}: {t.Text}"));
+        Assert.NotEqual("Identifier: " + source, tokens);
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidIdentifiers))]
+    public void ValidIdentifierTest(string source)
+    {
+        var tokens = string.Join("||", GetTokens(source).Select(t => $"{t.Kind}: {t.Text}"));
+        Assert.Equal("Identifier: " + source, tokens);
+    }
+
+    public static IEnumerable<object[]> InvalidIdentifiers()
+    {
+        yield return new object[] { "main$" };
+        yield return new object[] { "main#" };
+        yield return new object[] { "main+" };
+        yield return new object[] { "main&" };
+        yield return new object[] { "main&" };
+        yield return new object[] { "main&" };
+        yield return new object[] { "Ä" };
+        yield return new object[] { "\u0410" }; // Cyrillic A
+        yield return new object[] { "\u0100" }; // Latin Ā
+        yield return new object[] { "9G" };
+        //yield return new object[] { "extremely_long_identifier_of_more_then_31_character" };
+    }
+
+    public static IEnumerable<object[]> ValidIdentifiers()
+    {
+        yield return new object[] { "main_" };
+        yield return new object[] { "MAIN" };
+        yield return new object[] { "MAIN20002" };
+    }
+
 }
