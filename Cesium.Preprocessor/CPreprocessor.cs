f38d6d55cfd4ed4ef2185f70d58ea0bbd011eca7
Friedrich von Never <friedrich@fornever.me>
2021-12-24 14:29:34 +0700

    (#21) Preprocessor: basic directive API

--- Cesium.Preprocessor/CPreprocessor.cs
+++ Cesium.Preprocessor/CPreprocessor.cs
@@ -6,15 +6,6 @@ namespace Cesium.Preprocessor;
 
 public record CPreprocessor(ILexer<IToken<CPreprocessorTokenType>> Lexer)
 {
-    private IEnumerable<IToken<CPreprocessorTokenType>> GetPreprocessingResults()
-    {
-        var stream = Lexer.ToStream();
-        while (!stream.IsEnd)
-        {
-            yield return stream.Consume();
-        }
-    }
-
     public string ProcessSource()
     {
         var buffer = new StringBuilder();
@@ -24,5 +15,79 @@ public record CPreprocessor(ILexer<IToken<CPreprocessorTokenType>> Lexer)
         }
 
         return buffer.ToString();
+    }
+
+    private IEnumerable<IToken<CPreprocessorTokenType>> GetPreprocessingResults()
+    {
+        var newLine = true;
+
+        var stream = Lexer.ToStream();
+        while (!stream.IsEnd)
+        {
+            var token = stream.Consume();
+            switch (token.Kind)
+            {
+                case CPreprocessorTokenType.End:
+                    yield break;
+
+                case CPreprocessorTokenType.WhiteSpace:
+                case CPreprocessorTokenType.Comment:
+                    yield return token;
+                    break;
+
+                case CPreprocessorTokenType.NewLine:
+                    newLine = true;
+                    yield return token;
+                    break;
+
+                case CPreprocessorTokenType.Hash:
+                    if (newLine)
+                    {
+                        // TODO: Recursive processing
+                        foreach (var t in ProcessDirective(ReadDirectiveLine(token, stream)))
+                            yield return t;
+                    }
+
+                    newLine = false;
+                    break;
+
+                case CPreprocessorTokenType.Error:
+                case CPreprocessorTokenType.DoubleHash:
+                case CPreprocessorTokenType.HeaderName:
+                case CPreprocessorTokenType.PreprocessingToken:
+                    newLine = false;
+                    yield return token;
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Illegal token {token.Kind} {token.Text}.");
+            }
+        }
+    }
+
+    private IEnumerable<IToken<CPreprocessorTokenType>> ProcessDirective(
+        IEnumerable<IToken<CPreprocessorTokenType>> directiveTokens)
+    {
+        return directiveTokens;
+    }
+
+    private IEnumerable<IToken<CPreprocessorTokenType>> ReadDirectiveLine(
+        IToken<CPreprocessorTokenType> firstToken,
+        IStream<IToken<CPreprocessorTokenType>> stream)
+    {
+        yield return firstToken;
+        while (!stream.IsEnd)
+        {
+            var token = stream.Consume();
+            switch (token.Kind)
+            {
+                case CPreprocessorTokenType.NewLine:
+                case CPreprocessorTokenType.End:
+                    yield return token;
+                    yield break;
+                default:
+                    yield return token;
+            }
+        }
     }
 }
