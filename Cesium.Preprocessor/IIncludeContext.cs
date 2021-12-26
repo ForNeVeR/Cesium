e76d8456e9c6473011c936b951044a30fcd68e50
Friedrich von Never <friedrich@fornever.me>
2021-12-26 15:20:42 +0700

    (#21) IIncludeContext: code style fix

--- Cesium.Preprocessor/IIncludeContext.cs
+++ Cesium.Preprocessor/IIncludeContext.cs
@@ -2,6 +2,6 @@ namespace Cesium.Preprocessor;
 
 public interface IIncludeContext
 {
-    public ValueTask<TextReader> LookUpAngleBracedIncludeFile(string filePath);
-    public ValueTask<TextReader> LookUpQuotedIncludeFile(string filePath);
+    ValueTask<TextReader> LookUpAngleBracedIncludeFile(string filePath);
+    ValueTask<TextReader> LookUpQuotedIncludeFile(string filePath);
 }
