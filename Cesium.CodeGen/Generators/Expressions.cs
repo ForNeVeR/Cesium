8b01ab2f4a60736cd61dcb12a9723cb96c909e05
Friedrich von Never <friedrich@fornever.me>
2022-01-01 00:42:49 +0700

    CodeGen: fixes for string constants

--- Cesium.CodeGen/Generators/Expressions.cs
+++ Cesium.CodeGen/Generators/Expressions.cs
@@ -98,6 +98,6 @@ internal static class Expressions
     private static void EmitStringConstantExpression(FunctionScope scope, StringConstantExpression expression)
     {
         var fieldReference = scope.AssemblyContext.GetConstantPoolReference(expression.ConstantContent);
-        scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldflda, fieldReference));
+        scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldsflda, fieldReference));
     }
 }
