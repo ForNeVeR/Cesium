60cfc77e2c8c636bb3f021c99965c91c3fba12bf
Andrii Kurdiumov <kant2002@gmail.com>
2021-12-31 19:06:30 +0600

    Allow use runtime on .NET Standard 2.0

--- Cesium.Runtime/StdIoFunctions.cs
+++ Cesium.Runtime/StdIoFunctions.cs
@@ -1,14 +1,35 @@
 namespace Cesium.Runtime;
 
 using System.Runtime.InteropServices;
+using System.Text;
 
 /// <summary>
 /// Functions declared in the stdio.h
 /// </summary>
 public unsafe static class StdIoFunctions
 {
-    public static void PutS(byte* format)
+    public static void PutS(byte* str)
     {
-        Console.Write(Marshal.PtrToStringUTF8((nint)format));
+#if NETSTANDARD
+        Encoding encoding = Encoding.UTF8;
+        int byteLength = 0;
+        byte* search = str;
+        while (*search != '\0')
+        {
+            byteLength++;
+            search++;
+        }
+
+        int stringLength = encoding.GetCharCount(str, byteLength);
+        string s = new string('\0', stringLength);
+        fixed (char* pTempChars = s)
+        {
+            encoding.GetChars(str, byteLength, pTempChars, stringLength);
+        }
+
+        Console.Write(s);
+#else
+        Console.Write(Marshal.PtrToStringUTF8((nint)str));
+#endif
     }
 }
--- Cesium.Runtime/StringFunctions.cs
+++ Cesium.Runtime/StringFunctions.cs
@@ -1,6 +1,8 @@
 namespace Cesium.Runtime;
 
+using System.Text;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 /// <summary>
 /// Functions declared in the string.h
@@ -9,6 +11,20 @@ public unsafe static class StringFunctions
 {
     public static uint StrLen(byte* str)
     {
+#if NETSTANDARD
+        Encoding encoding = Encoding.UTF8;
+        int byteLength = 0;
+        byte* search = str;
+        while (*search != '\0')
+        {
+            byteLength++;
+            search++;
+        }
+
+        int stringLength = encoding.GetCharCount(str, byteLength);
+        return (uint)stringLength;
+#else
         return (uint)(Marshal.PtrToStringUTF8((nint)str)?.Length ?? 0);
+#endif
     }
 }
