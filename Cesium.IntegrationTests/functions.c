7a8bf7f44c0ae94cf28a7730c3ae976f26ab865d
Andrii Kurdiumov <kant2002@gmail.com>
2022-02-13 22:21:40 +0600

    Add more test cases for function signatures

--- Cesium.IntegrationTests/functions.c
+++ Cesium.IntegrationTests/functions.c
@@ -1,6 +1,26 @@
-int foo(void)
+//void forward_declaration_void_1();
+//void forward_declaration_void_2(void);
+
+void declaration_void(void)
 {
+}
+
+int foo()
+{
+    declaration_void();
+    //forward_declaration_void_1();
+    //forward_declaration_void_2();
     return 42;
+}
+
+void forward_declaration_void_1()
+{
+    // Do nothing here.
+}
+
+void forward_declaration_void_2()
+{
+    // Do nothing here.
 }
 
 int main(void)
