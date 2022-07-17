94ed951ac10b1433e40406f50c1ae6487e592408
Phantom <P_hanto_m@outlook.com>
2022-07-17 20:16:51 +0400

    fix errors

--- Cesium.Compiler/Main.cs
+++ Cesium.Compiler/Main.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 using Cesium.CodeGen;
 using Cesium.Compiler;
 using CommandLine;
+using CommandLine.Text;
 
 var parserResult = new Parser(x => x.HelpWriter = null).ParseArguments<Arguments>(args);
 
@@ -35,10 +36,10 @@ return await parserResult.MapResult(async args =>
 
 static void DisplayHelp<T>(ParserResult<T> result)
 {
-    var helpText = CommandLine.Text.HelpText.AutoBuild(result, h =>
+    var helpText = HelpText.AutoBuild(result, h =>
     {
         h.AddEnumValuesToHelpText = true;
-        return h;
+        return HelpText.DefaultParsingErrorsHandler(result, h);
     }, e => e);
     Console.WriteLine(helpText);
 }
