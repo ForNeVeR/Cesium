ffe9dd905057373e7d85a85594fdc6dc46193898
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-06 17:18:44 +0600

    Address PR feedback

--- Cesium.CodeGen/Ir/Types/CTypeSystem.cs
+++ Cesium.CodeGen/Ir/Types/CTypeSystem.cs
@@ -1,4 +1,4 @@
-using Mono.Cecil;
+using Cesium.CodeGen.Extensions;
 
 namespace Cesium.CodeGen.Ir.Types;
 
@@ -7,5 +7,5 @@ internal class CTypeSystem
     public IType Boolean { get; } = new PrimitiveType(PrimitiveTypeKind.Int);
     public IType Char { get; } = new PrimitiveType(PrimitiveTypeKind.Char);
     public IType Int { get; } = new PrimitiveType(PrimitiveTypeKind.Int);
-    public IType CharPtr { get; } = new PointerType(new PrimitiveType(PrimitiveTypeKind.Char));
+    public IType CharPtr { get; } = new PrimitiveType(PrimitiveTypeKind.Char).MakePointerType();
 }
