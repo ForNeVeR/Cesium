cabea7fa060c8b299cad6de6de75c6da1e96338a
BoundedChenn31 <BoundedChenn31@gmail.com>
2022-08-15 23:22:49 +0300

    Warn when running approve-all.ps1 not in script mode

--- scripts/approve-all.ps1
+++ scripts/approve-all.ps1
@@ -5,8 +5,13 @@ param (
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-Get-ChildItem -Recurse $SolutionRoot -Filter "*.received.txt" | ForEach-Object {
-    $receivedTestResult = $_.FullName
-    $approvedTestResult = $receivedTestResult.Replace('.received.txt', '.verified.txt')
-    Move-Item -Force -LiteralPath $receivedTestResult $approvedTestResult
+if (-Not $PSScriptRoot) {
+    Write-Error "\`$PSScriptRoot variable isn't set. Do you run this code as a script? Please, follow instructions at https://github.com/ForNeVeR/Cesium/blob/main/docs/tests.md"
+}
+else {
+    Get-ChildItem -Recurse $SolutionRoot -Filter "*.received.txt" | ForEach-Object {
+        $receivedTestResult = $_.FullName
+        $approvedTestResult = $receivedTestResult.Replace('.received.txt', '.verified.txt')
+        Move-Item -Force -LiteralPath $receivedTestResult $approvedTestResult
+    }
 }
