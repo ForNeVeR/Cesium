813f7f0e39a366a5e74f201c3dc80727c3e19b06
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-19 09:53:35 +0600

    Support basic #define replacement

--- Cesium.Compiler/stdlib/stdlib.h
+++ Cesium.Compiler/stdlib/stdlib.h
@@ -1,3 +1,6 @@
+#define INT_MIN -2147483648
+#define INT_MAX 2147483647
+
 __cli_import("Cesium.Runtime.StdLibFunctions::Abs")
 int abs(int n);
 
--- Cesium.Preprocessor/IMacroContext.cs
+++ Cesium.Preprocessor/IMacroContext.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Cesium.Preprocessor;
 
 public interface IMacroContext
 {
-    bool TryResolveMacro(string macro, out string? macroReplacement);
+    bool TryResolveMacro(string macro, [NotNullWhen(true)]out string? macroReplacement);
 
     void DefineMacro(string macro, string? replacement);
 }
