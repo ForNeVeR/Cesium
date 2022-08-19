7bbf61aae65ef5f6b3214916be750b52f565d306
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-19 23:30:51 +0600

    do not have code in headers

--- Cesium.Compiler/stdlib/stdio.h
+++ Cesium.Compiler/stdlib/stdio.h
@@ -1,8 +1,5 @@
 __cli_import("Cesium.Runtime.StdIoFunctions::PutS")
 void puts(char *s); // TODO[#156]: Change to int
 
-void printf(char* s) {
-    // That's temporary until varargs would not be implemented.
-    // no formatting obviously.
-    puts(s);
-}
+__cli_import("Cesium.Runtime.StdIoFunctions::PutS")
+void printf(char* s);
