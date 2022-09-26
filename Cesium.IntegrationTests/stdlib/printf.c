230fee990a0065334824539ac7d84a13646a2592
Andrii Kurdiumov <kant2002@gmail.com>
2022-09-26 20:50:44 +0800

    Add %lu and %p format

--- Cesium.IntegrationTests/stdlib/printf.c
+++ Cesium.IntegrationTests/stdlib/printf.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
     // Print variables
     int intValue = 5;
     printf("%d\n", intValue);
+    printf("%u\n", -1);
+    printf("%lu\n", -1);
 
     float floatValue = 5.99;
     printf("%f\n", floatValue);
@@ -18,6 +20,9 @@ int main(int argc, char *argv[])
 
     char myLetter = 'D';
     printf("%c\n%c", myLetter, '1');
+    // We cannot validate this automatically, but at least we can uncomment and check
+    // that this produce appropriate results.
+    //printf("%p", &doubleValue);
 
     return 42;
 }
