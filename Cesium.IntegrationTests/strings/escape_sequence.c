d60e165db48dfbb4c7847008d5de272cc9aa1519
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2022-10-03 01:30:59 +0300

    added support of simple escape sequences and numeric escape sequences

--- Cesium.IntegrationTests/strings/escape_sequence.c
+++ Cesium.IntegrationTests/strings/escape_sequence.c
@@ -5,7 +5,9 @@ int main(int argc, char *argv[])
 {
     printf("\'\"\?\\\a\b\f\n\r\t\v");
     printf("\023");
+    printf("\077");
     printf("\xF0");
+    printf("\xFF");
     printf("\u2200");
     printf("\U0001f34c");
     return 42;
--- Cesium.Parser/TokenExtensions.cs
+++ Cesium.Parser/TokenExtensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.RegularExpressions;
 using Cesium.Core;
 using Yoakke.SynKit.C.Syntax;
 using Yoakke.SynKit.Lexer;
@@ -11,7 +13,33 @@ public static class TokenExtensions
         if (token.Kind != CTokenType.StringLiteral)
             throw new ParseException($"Non-string literal token: {token.Kind} {token.Text}");
 
-        // TODO[#235]: More thorough unwrap for more literal types.
-        return token.Text.Trim('"').Replace("\\n", "\n");
+        // simple escape sequences
+        var result = token.Text.Trim('"')
+            .Replace("\\'", "\'")
+            .Replace("\\\"", "\"")
+            .Replace("\\?", "?")
+            .Replace("\\\\", "\\")
+            .Replace("\\a", "\a")
+            .Replace("\\b", "\b")
+            .Replace("\\f", "\f")
+            .Replace("\\n", "\n")
+            .Replace("\\r", "\r")
+            .Replace("\\t", "\t")
+            .Replace("\\v", "\v");
+
+        // numeric escape sequences
+        result = Regex.Replace(result, @"\\([0-7]{3})", m =>
+            Convert.ToChar(Convert.ToUInt32(m.Groups[1].Value, 8)).ToString());
+
+        result = Regex.Replace(result, @"\\x([0-9a-fA-F]{2})", m =>
+            Convert.ToChar(Convert.ToUInt32(m.Groups[1].Value, 16)).ToString());
+
+        //todo: universal character names
+
+        //result = Regex.Replace(result, @"(\\u[0-9a-fA-F]{4})", "$1");
+
+        //result = Regex.Replace(result, @"(\\U[0-9a-fA-F]{8})", "$1");
+
+        return result;
     }
 }
