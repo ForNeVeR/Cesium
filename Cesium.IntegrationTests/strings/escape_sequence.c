db12132c9fe413ecf4a88780bcc337b276fd94aa
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2022-10-03 20:32:46 +0300

    universal character names support

--- Cesium.IntegrationTests/strings/escape_sequence.c
+++ Cesium.IntegrationTests/strings/escape_sequence.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     printf("\'\"\?\\\a\b\f\n\r\t\v");
-    printf("\023");
+
+    printf("\024");
+    printf("\007");
     printf("\077");
+
+    printf("\x06");
+    printf("\x28");
     printf("\xF0");
     printf("\xFF");
+
     printf("\u2200");
     printf("\U0001f34c");
+
     return 42;
 }
