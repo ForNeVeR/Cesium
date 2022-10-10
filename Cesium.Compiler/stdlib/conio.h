2335e30d1a1b0988556c142ead1cdc1069c2fab3
Andrii Kurdiumov <kant2002@gmail.com>
2022-10-10 23:21:44 +0800

    Add non-standard function These functions appears in the tutorial, so I would like to have them

---
+++ Cesium.Compiler/stdlib/conio.h
@@ -0,0 +1,8 @@
+__cli_import("Cesium.Runtime.ConioFunctions::KbHit")
+int kbhit(void);
+
+__cli_import("Cesium.Runtime.ConioFunctions::GetCh")
+int getch(void);
+
+__cli_import("Cesium.Runtime.ConioFunctions::Cls")
+void cls(void);
