a8c2a199ba8dca30054d9fb7023b28f441815c1a
Friedrich von Never <friedrich@fornever.me>
2022-11-13 21:24:28 +0100

    (#236) RuntimeConfig: use C# 11 raw string literal

--- Cesium.Compiler/RuntimeConfig.cs
+++ Cesium.Compiler/RuntimeConfig.cs
@@ -2,13 +2,15 @@ namespace Cesium.Compiler;
 
 internal static class RuntimeConfig
 {
-    public static string EmitNet6() => @"{
-  ""runtimeOptions"": {
-    ""tfm"": ""net6.0"",
-    ""framework"": {
-      ""name"": ""Microsoft.NETCore.App"",
-      ""version"": ""6.0.0""
+    public static string EmitNet6() => """
+    {
+      "runtimeOptions": {
+        "tfm": "net6.0",
+        "framework": {
+          "name": "Microsoft.NETCore.App",
+          "version": "6.0.0"
+        }
+      }
     }
-  }
-}".ReplaceLineEndings("\n");
+    """.ReplaceLineEndings("\n");
 }
