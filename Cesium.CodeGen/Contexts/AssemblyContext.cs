93b7e3443f47e758ced02c00426aa95fc2d4e111
Andrii Kurdiumov <kant2002@gmail.com>
2023-05-05 13:08:01 +0600

    Create bytearray wrapper

--- Cesium.CodeGen/Contexts/AssemblyContext.cs
+++ Cesium.CodeGen/Contexts/AssemblyContext.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using Cesium.CodeGen.Contexts.Meta;
 using Cesium.CodeGen.Extensions;
@@ -66,7 +67,7 @@ public class AssemblyContext
     public const string ConstantPoolTypeName = "<ConstantPool>";
 
     private readonly Dictionary<int, TypeReference> _stubTypesPerSize = new();
-    private readonly Dictionary<string, FieldReference> _dataConstantHolders = new();
+    private readonly Dictionary<ByteArrayWrapper, FieldReference> _dataConstantHolders = new(new ByteArrayEqualityComparer());
 
     private readonly Lazy<TypeDefinition> _constantPool;
     private MethodDefinition? _globalInitializer;
@@ -155,46 +156,26 @@ public class AssemblyContext
 
     public FieldReference GetConstantPoolReference(string stringConstant)
     {
-        if (_dataConstantHolders.TryGetValue(stringConstant, out var field))
-            return field;
-
         var encoding = Encoding.UTF8;
         var bufferSize = encoding.GetByteCount(stringConstant) + 1;
         var data = new byte[bufferSize];
         var writtenBytes = encoding.GetBytes(stringConstant, data);
         Debug.Assert(writtenBytes == bufferSize - 1);
-
-        var type = GetStubType(bufferSize);
-        field = GenerateFieldForDataConstant(type, data);
-        _dataConstantHolders.Add(stringConstant, field);
-
-        return field;
+        return GetConstantPoolReference(data);
     }
 
     public FieldReference GetConstantPoolReference(byte[] dataConstant)
     {
-        if (dataConstant.Length >= 10)
-        {
-            // Do not attempt to put large arrays in cache.
-            // Most likely they are single use in the application.
-            // No chance that they would be cached by values.
-            var bufferSize = dataConstant.Length;
-            var type = GetStubType(bufferSize);
-            return GenerateFieldForDataConstant(type, dataConstant);
-        }
-        else
-        {
-            var hash = string.Join("", dataConstant.Select(_ => _.ToString("X2")));
-            if (_dataConstantHolders.TryGetValue(hash, out var field))
-                return field;
-
-            var bufferSize = dataConstant.Length;
-            var type = GetStubType(bufferSize);
-            field = GenerateFieldForDataConstant(type, dataConstant);
-            _dataConstantHolders.Add(hash, field);
-
+        var wrapper = new ByteArrayWrapper(dataConstant);
+        if (_dataConstantHolders.TryGetValue(wrapper, out var field))
             return field;
-        }
+
+        var bufferSize = dataConstant.Length;
+        var type = GetStubType(bufferSize);
+        field = GenerateFieldForDataConstant(type, dataConstant);
+        _dataConstantHolders.Add(wrapper, field);
+
+        return field;
     }
 
     private TypeReference GetStubType(int size)
@@ -235,5 +216,103 @@ public class AssemblyContext
         var constantPool = _constantPool.Value;
         constantPool.Fields.Add(field);
         return field;
+    }
+
+    private class ByteArrayEqualityComparer : IEqualityComparer<ByteArrayWrapper>
+    {
+        public bool Equals(ByteArrayWrapper? x, ByteArrayWrapper? y)
+        {
+            if (x is null && y is null)
+            {
+                return true;
+            }
+
+            if (x is null || y is null)
+            {
+                return false;
+            }
+
+            if (x.Value.Length != y.Value.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < x.Value.Length; i++)
+            {
+                if (x.Value[i] != y.Value[i]) return false;
+            }
+
+            return true;
+        }
+
+        public int GetHashCode([DisallowNull] ByteArrayWrapper obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    private class ByteArrayWrapper
+    {
+        public ByteArrayWrapper(byte[] value)
+        {
+            Value = value;
+        }
+        public byte[] Value { get; }
+
+        private int? hash;
+
+        public override int GetHashCode()
+        {
+            // Borrowed from https://github.com/jitbit/MurmurHash.net/blob/master/MurmurHash.cs
+            int length = Value.Length;
+            if (length == 0)
+                return 0;
+
+            if (hash.HasValue)
+                return hash.Value;
+
+            uint seed = 0xc58f1a7a;
+            const uint m = 0x5bd1e995;
+            const int r = 24;
+
+            uint h = seed ^ (uint)length;
+            int currentIndex = 0;
+            while (length >= 4)
+            {
+                uint k = (uint)(Value[currentIndex++] | Value[currentIndex++] << 8 | Value[currentIndex++] << 16 | Value[currentIndex++] << 24);
+                k *= m;
+                k ^= k >> r;
+                k *= m;
+
+                h *= m;
+                h ^= k;
+                length -= 4;
+            }
+            switch (length)
+            {
+                case 3:
+                    h ^= (ushort)(Value[currentIndex++] | Value[currentIndex++] << 8);
+                    h ^= (uint)(Value[currentIndex] << 16);
+                    h *= m;
+                    break;
+                case 2:
+                    h ^= (ushort)(Value[currentIndex++] | Value[currentIndex] << 8);
+                    h *= m;
+                    break;
+                case 1:
+                    h ^= Value[currentIndex];
+                    h *= m;
+                    break;
+                default:
+                    break;
+            }
+
+            h ^= h >> 13;
+            h *= m;
+            h ^= h >> 15;
+
+            hash = unchecked((int)h);
+            return hash.Value;
+        }
     }
 }
