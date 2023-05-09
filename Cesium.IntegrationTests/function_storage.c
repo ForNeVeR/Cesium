eb4523318915393f92ad53e07fab98d75a147b62
Andrii Kurdiumov <kant2002@gmail.com>
2023-05-10 00:13:51 +0600

    Restore back not functional change.

--- Cesium.IntegrationTests/function_storage.c
+++ Cesium.IntegrationTests/function_storage.c
@@ -14,17 +14,18 @@ void storage_declared_worker()
     printf("inside storage_declared_worker");
 }
 
+/*
 void storage_defined_worker();
 
 static void storage_defined_worker()
 {
     printf("inside storage_defined_worker");
-}
+}*/
 
 int main(void)
 {
     worker();
     storage_declared_worker();
-    storage_defined_worker();
+    //storage_defined_worker();
     return 42;
 }
