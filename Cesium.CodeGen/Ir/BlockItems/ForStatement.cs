ebde75bcb63e9b0b7313415bba172f7adcdc976a
Andrii Kurdiumov <kant2002@gmail.com>
2023-05-15 11:04:20 +0600

    Add support for most common use of for

--- Cesium.CodeGen/Ir/BlockItems/ForStatement.cs
+++ Cesium.CodeGen/Ir/BlockItems/ForStatement.cs
@@ -1,11 +1,13 @@
 using Cesium.CodeGen.Contexts;
 using Cesium.CodeGen.Extensions;
 using Cesium.CodeGen.Ir.Expressions;
+using Cesium.Core;
 
 namespace Cesium.CodeGen.Ir.BlockItems;
 
 internal class ForStatement : LoopStatement, IBlockItem
 {
+    private readonly IBlockItem? _initDeclaration;
     private readonly IExpression? _initExpression;
     private readonly IExpression? _testExpression;
     private readonly IExpression? _updateExpression;
@@ -13,8 +15,13 @@ internal class ForStatement : LoopStatement, IBlockItem
 
     public ForStatement(Ast.ForStatement statement)
     {
-        var (initExpression, testExpression, updateExpression, body) = statement;
+        var (initDeclaration, initExpression, testExpression, updateExpression, body) = statement;
+        _initDeclaration = initDeclaration?.ToIntermediate();
         _initExpression = initExpression?.ToIntermediate();
+
+        if (_initDeclaration != null && _initExpression != null)
+            throw new CompilationException("for statement: can't have both init declaration and expression");
+
         _testExpression = testExpression?.ToIntermediate();
         _updateExpression = updateExpression?.ToIntermediate();
         _body = body.ToIntermediate();
@@ -28,7 +35,7 @@ internal class ForStatement : LoopStatement, IBlockItem
 
         return MakeLoop(
             loopScope,
-            new ExpressionStatement(_initExpression),
+            _initDeclaration ?? new ExpressionStatement(_initExpression),
             _testExpression,
             _updateExpression,
             _body,
