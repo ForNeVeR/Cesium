a6e19d302815d84e6265acc9f9013d32044713d5
невидимка <bessonov.do@perfema.com>
2023-05-21 00:41:24 +0300

    Unify FunctionCall and IndirectFunctionCall argument emitting

--- Cesium.CodeGen/Ir/Expressions/IndirectFunctionCallExpression.cs
+++ Cesium.CodeGen/Ir/Expressions/IndirectFunctionCallExpression.cs
@@ -1,79 +1,32 @@
 using Cesium.CodeGen.Contexts;
-using Cesium.CodeGen.Contexts.Meta;
-using Cesium.CodeGen.Extensions;
 using Cesium.CodeGen.Ir.Types;
 using Cesium.Core;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
-using Mono.Cecil.Rocks;
 
 namespace Cesium.CodeGen.Ir.Expressions;
 
-internal class IndirectFunctionCallExpression : IExpression
+internal class IndirectFunctionCallExpression : FunctionCallExpressionBase
 {
-    private readonly IdentifierExpression _function;
     private readonly IReadOnlyList<IExpression> _arguments;
     private readonly IExpression _callee;
     private readonly FunctionType _calleeType;
 
-    public IndirectFunctionCallExpression(IdentifierExpression function, IExpression callee, FunctionType calleeType, IReadOnlyList<IExpression> arguments)
+    public IndirectFunctionCallExpression(IExpression callee, FunctionType calleeType, IReadOnlyList<IExpression> arguments)
     {
-        _function = function;
         _arguments = arguments;
         _callee = callee;
         _calleeType = calleeType;
     }
 
-    public IExpression Lower(IDeclarationScope scope) => this;
+    public override IExpression Lower(IDeclarationScope scope) => this;
 
-    public void EmitTo(IEmitScope scope)
+    public override void EmitTo(IEmitScope scope)
     {
-        if (_calleeType.Parameters?.IsVarArg == true)
-            throw new CompilationException("Vararg is not supported in function pointers");
-
         if (_callee == null)
             throw new AssertException("Should be lowered");
 
-        VariableDefinition? varArgBuffer = null;
-        var explicitParametersCount = _calleeType.Parameters?.Parameters.Count ?? 0;
-        var varArgParametersCount = _arguments.Count - explicitParametersCount;
-        if (_calleeType.Parameters?.IsVarArg == true)
-        {
-            // TODO: See https://github.com/ForNeVeR/Cesium/issues/285
-            // Using sparse population of the parameters on the stack. 8 bytes should be enough for anybody.
-            // Also we need perform localloc on empty stack, so we will use local variable to save vararg buffer to temporary variable.
-            if (varArgParametersCount == 0)
-            {
-                scope.AddInstruction(OpCodes.Ldnull);
-            }
-            else
-            {
-                scope.AddInstruction(OpCodes.Ldc_I4, varArgParametersCount * 8);
-                scope.AddInstruction(OpCodes.Localloc);
-            }
-
-            varArgBuffer = new VariableDefinition(scope.Context.TypeSystem.Void.MakePointerType());
-            scope.Method.Body.Variables.Add(varArgBuffer);
-            scope.AddInstruction(OpCodes.Stloc, varArgBuffer);
-        }
-
-        foreach (var argument in _arguments.Take(explicitParametersCount))
-            argument.EmitTo(scope);
-
-        if (_calleeType.Parameters?.IsVarArg == true)
-        {
-            for (var i = 0; i < varArgParametersCount; i++)
-            {
-                var argument = _arguments[i + explicitParametersCount];
-                scope.AddInstruction(OpCodes.Ldloc, varArgBuffer!);
-                scope.AddInstruction(OpCodes.Ldc_I4, i * 8);
-                scope.AddInstruction(OpCodes.Add);
-                argument.EmitTo(scope);
-                scope.AddInstruction(OpCodes.Stind_I);
-            }
-
-            scope.AddInstruction(OpCodes.Ldloc, varArgBuffer!);
-        }
+        EmitArgumentList(scope, _calleeType.Parameters, _arguments);
 
         _callee.EmitTo(scope);
 
@@ -85,12 +38,17 @@ internal class IndirectFunctionCallExpression : IExpression
             {
                 callSite.Parameters.Add(new ParameterDefinition(param.Type.Resolve(scope.Context)));
             }
+
+            if (_calleeType.Parameters.IsVarArg)
+            {
+                callSite.Parameters.Add(new ParameterDefinition(scope.Context.TypeSystem.IntPtr));
+            }
         }
 
         scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Calli, callSite));
     }
 
-    public IType GetExpressionType(IDeclarationScope scope)
+    public override IType GetExpressionType(IDeclarationScope scope)
     {
         return _calleeType.ReturnType;
     }
