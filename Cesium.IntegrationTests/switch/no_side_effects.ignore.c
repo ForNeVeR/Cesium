388960768bdaf14748b356f43007241e0ff18981
Friedrich von Never <friedrich@fornever.me>
2023-05-20 21:54:32 +0200

    (#409) Connect the issue with a TODO

--- Cesium.IntegrationTests/switch/no_side_effects.ignore.c
+++ Cesium.IntegrationTests/switch/no_side_effects.ignore.c
@@ -2,7 +2,7 @@ int main(int argc, char *argv[])
 {
     int i = 0;
 
-    // bad SetValueExpression results in unbalanced stack (make issue for me!)
+    // TODO[#409]: bad SetValueExpression results in unbalanced stack
     switch (++i)
     {
         case 0:
