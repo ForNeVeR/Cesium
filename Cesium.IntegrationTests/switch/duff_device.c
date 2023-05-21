4ab1129001060f26941e9ade3d8750bca5635f36
Friedrich von Never <friedrich@fornever.me>
2023-05-21 22:04:06 +0200

    (#411) Codegen: mark the issue as fixed

--- Cesium.IntegrationTests/switch/duff_device.c
+++ Cesium.IntegrationTests/switch/duff_device.c
@@ -3,11 +3,7 @@ int main()
     int j = 0;
     int c = 42;
 
-    // TODO[#411] assignments are broken as expressions currently
-    // switch(c&3) while((c-=4)>=0) {
-    switch(c&3) while(c >= 4) {
-        c -= 4;
-
+    switch(c&3) while((c-=4)>=0) {
         j++; case 3:
         j++; case 2:
         j++; case 1:
