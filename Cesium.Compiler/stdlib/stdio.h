4df5dfe34164e749aea4eb5716e7c25e822d33cb
Andrii Kurdiumov <kant2002@gmail.com>
2023-10-09 09:37:38 +0600

    Add atoi/fprintf Also have to lower expression passed to varargs before performing implicit cast

--- Cesium.Compiler/stdlib/stdio.h
+++ Cesium.Compiler/stdlib/stdio.h
@@ -1,18 +1,21 @@
 #pragma once
+typedef void FILE;
+
+#define stdin 0
+#define stdout 1
+#define stderr 2
+
 __cli_import("Cesium.Runtime.StdIoFunctions::PutS")
 int puts(char *s);
 
 __cli_import("Cesium.Runtime.StdIoFunctions::PrintF")
 int printf(char* s, ...);
 
+__cli_import("Cesium.Runtime.StdIoFunctions::FPrintF")
+int fprintf(FILE* stream, char* s, ...);
+
 __cli_import("Cesium.Runtime.StdIoFunctions::PutChar")
 int putchar(char _Character);
-
-typedef void FILE;
-
-#define stdin 0
-#define stdout 1
-#define stderr 2
 
 __cli_import("Cesium.Runtime.StdIoFunctions::PutC")
 int putc(char _Character, FILE* stream);
