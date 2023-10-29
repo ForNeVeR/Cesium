7d51a343605b0a936f04ea2ccf7194b2a8607420
Lehonti Ramos <lehonti@ramos>
2023-10-30 00:47:25 +0100

    `sealed` expression types

--- Cesium.Ast/Expressions.cs
+++ Cesium.Ast/Expressions.cs
@@ -7,47 +7,47 @@ namespace Cesium.Ast;
 public abstract record Expression;
 
 // 6.4.5 String literals
-public record StringLiteralListExpression(ImmutableArray<IToken<CTokenType>> ConstantList) : Expression;
+public sealed record StringLiteralListExpression(ImmutableArray<IToken<CTokenType>> ConstantList) : Expression;
 
 // 6.5.1 Primary expressions
-public record IdentifierExpression(string Identifier) : Expression;
-public record ConstantLiteralExpression(IToken<CTokenType> Constant) : Expression;
-public record ParenExpression(Expression Contents) : Expression;
+public sealed record IdentifierExpression(string Identifier) : Expression;
+public sealed record ConstantLiteralExpression(IToken<CTokenType> Constant) : Expression;
+public sealed record ParenExpression(Expression Contents) : Expression;
 
 // 6.5.2 Postfix operators
-public record SubscriptingExpression(Expression Base, Expression Index) : Expression;
-public record FunctionCallExpression(Expression Function, ImmutableArray<Expression>? Arguments) : Expression;
-public record TypeCastOrNamedFunctionCallExpression(string TypeOrFunctionName, ImmutableArray<Expression> Arguments) : Expression;
-public record MemberAccessExpression(Expression Target, IdentifierExpression Identifier) : Expression;
-public record PointerMemberAccessExpression(Expression Target, IdentifierExpression Identifier) : Expression;
-public record PostfixIncrementDecrementExpression(IToken<CTokenType> PrefixOperator, Expression Target) : Expression;
+public sealed record SubscriptingExpression(Expression Base, Expression Index) : Expression;
+public sealed record FunctionCallExpression(Expression Function, ImmutableArray<Expression>? Arguments) : Expression;
+public sealed record TypeCastOrNamedFunctionCallExpression(string TypeOrFunctionName, ImmutableArray<Expression> Arguments) : Expression;
+public sealed record MemberAccessExpression(Expression Target, IdentifierExpression Identifier) : Expression;
+public sealed record PointerMemberAccessExpression(Expression Target, IdentifierExpression Identifier) : Expression;
+public sealed record PostfixIncrementDecrementExpression(IToken<CTokenType> PrefixOperator, Expression Target) : Expression;
 
 // 6.5.3 Unary operators
-public record PrefixIncrementDecrementExpression(IToken<CTokenType> PrefixOperator, Expression Target) : Expression;
-public record UnaryOperatorExpression(string Operator, Expression Target) : Expression;
-public record IndirectionExpression(Expression Target) : Expression;
+public sealed record PrefixIncrementDecrementExpression(IToken<CTokenType> PrefixOperator, Expression Target) : Expression;
+public sealed record UnaryOperatorExpression(string Operator, Expression Target) : Expression;
+public sealed record IndirectionExpression(Expression Target) : Expression;
 
 // 6.5.3.4 The sizeof and _Alignof operators
-public record IdentifierSizeOfOperatorExpression(IdentifierExpression Identifier) : Expression;
+public sealed record IdentifierSizeOfOperatorExpression(IdentifierExpression Identifier) : Expression;
 
-public record TypeNameSizeOfOperatorExpression(TypeName TypeName) : Expression;
+public sealed record TypeNameSizeOfOperatorExpression(TypeName TypeName) : Expression;
 
 // 6.5.4 Cast expression
-public record CastExpression(TypeName TypeName, Expression Target) : Expression;
+public sealed record CastExpression(TypeName TypeName, Expression Target) : Expression;
 
 // 6.5.5–6.5.14: Various binary operators
 public record BinaryOperatorExpression(Expression Left, string Operator, Expression Right) : Expression;
-public record LogicalBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
+public sealed record LogicalBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
     : BinaryOperatorExpression(Left, Operator, Right);
-public record ArithmeticBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
+public sealed record ArithmeticBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
     : BinaryOperatorExpression(Left, Operator, Right);
-public record BitwiseBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
+public sealed record BitwiseBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
     : BinaryOperatorExpression(Left, Operator, Right);
-public record ComparisonBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
+public sealed record ComparisonBinaryOperatorExpression(Expression Left, string Operator, Expression Right)
     : BinaryOperatorExpression(Left, Operator, Right);
 
 // 6.5.15: Conditional operator
-public record ConditionalExpression(Expression Condition, Expression TrueExpression, Expression FalseExpression)
+public sealed record ConditionalExpression(Expression Condition, Expression TrueExpression, Expression FalseExpression)
     : Expression;
 
 // 6.5.16 Assignment operators
@@ -55,4 +55,4 @@ public record AssignmentExpression(Expression Left, string Operator, Expression 
     : BinaryOperatorExpression(Left, Operator, Right);
 
 // 6.5.17 Comma operator
-public record CommaExpression(Expression Left, Expression Right) : Expression;
+public sealed record CommaExpression(Expression Left, Expression Right) : Expression;
