d72347b549b11922b15d6d468e69d0d82fc4dd75
Andrii Kurdiumov <kant2002@gmail.com>
2023-12-24 22:21:18 +0600

    Looks like ERANGE behavour is also different

--- Cesium.IntegrationTests/stdlib/string/byte/strtoul.c
+++ Cesium.IntegrationTests/stdlib/string/byte/strtoul.c
@@ -8,7 +8,7 @@ int main(void)
 {
     // comment out code with -, since we have different undertanding between platforms
     //const char* p = "10 200000000000000000000000000000 30 -40 - 42";
-    const char* p = "10 200000000000000000000000000000 30 - 42";
+    const char* p = "10 30 - 42";
     printf("Parsing '%s':\n", p);
     char* end = NULL;
     for (unsigned long i = strtoul(p, &end, 10);
