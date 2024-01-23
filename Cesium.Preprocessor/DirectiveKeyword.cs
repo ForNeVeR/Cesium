61c7f900207bc67d61c606453d3445ff7cb865c4
Daniel <modde1311@gmail.com>
2024-01-23 13:23:51 +0300

    codestyle

---
+++ Cesium.Preprocessor/DirectiveKeyword.cs
@@ -0,0 +1,16 @@
+namespace Cesium.Preprocessor;
+
+public static class LiteralConstants
+{
+    public const string Include = "include";
+    public const string Error = "error";
+    public const string Define = "define";
+    public const string Undef = "undef";
+    public const string If = "if";
+    public const string IfDef = "ifdef";
+    public const string IfnDef = "ifndef";
+    public const string Elif = "elif";
+    public const string Endif = "endif";
+    public const string Else = "else";
+    public const string Pragma = "pragma";
+}
