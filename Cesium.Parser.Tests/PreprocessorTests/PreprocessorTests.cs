7022f83af6f26f2b6f1d6419ce0f20798d632bf2
Daniel <modde1311@gmail.com>
2024-01-26 00:43:27 +0300

    fix BinaryExpression && return internal modifiers && reside tests to Cesium.Parser.Tests for using lexer

--- Cesium.Parser.Tests/PreprocessorTests/PreprocessorTests.cs
+++ Cesium.Parser.Tests/PreprocessorTests/PreprocessorTests.cs
@@ -732,4 +732,49 @@ int x = __LINE__;
 @"
 char* x = __FILE__;
 ");
+
+    [Theory]
+    [InlineData("9", "==", "10", false)]
+    [InlineData("10", "==", "10", true)]
+
+    [InlineData("9", "!=", "10", true)]
+    [InlineData("10", "!=", "10", false)]
+
+    [InlineData("9", "<=", "9", true)]
+    [InlineData("9", "<=", "10", true)]
+    [InlineData("10", "<=", "9", false)]
+
+    [InlineData("9", ">=", "9", true)]
+    [InlineData("10", ">=", "9", true)]
+    [InlineData("9", ">=", "10", false)]
+
+    [InlineData("10", "<", "9", false)]
+    [InlineData("9", "<", "10", true)]
+    [InlineData("10", "<", "10", false)]
+
+    [InlineData("10", ">", "9", true)]
+    [InlineData("9", ">", "10", false)]
+    [InlineData("10", ">", "10", false)]
+
+    [InlineData("0", "&&", "0", false)]
+    [InlineData("0", "&&", "1", false)]
+    [InlineData("1", "&&", "0", false)]
+    [InlineData("1", "&&", "1", true)]
+
+    [InlineData("0", "||", "0", false)]
+    [InlineData("0", "||", "1", true)]
+    [InlineData("1", "||", "0", true)]
+    [InlineData("1", "||", "1", true)]
+    public async Task EvaluateExpressionAllVariants(
+        string firstValue,
+        string oper,
+        string secondValue,
+        bool expectedResult)
+    {
+        const string funcText = "int foo() { return 0; }";
+        var actualResult = await DoPreprocess($@"#if {firstValue} {oper} {secondValue}{Environment.NewLine}"
+                                        + funcText
+                                        + $"{Environment.NewLine}#endif");
+        Assert.Equal(expectedResult ? funcText : "", actualResult.Trim());
+    }
 }
--- Cesium.Preprocessor/BinaryExpression.cs
+++ Cesium.Preprocessor/BinaryExpression.cs
@@ -2,7 +2,7 @@ using Cesium.Core;
 
 namespace Cesium.Preprocessor;
 
-public class BinaryExpression(
+internal class BinaryExpression(
     IPreprocessorExpression first,
     CPreprocessorOperator @operator,
     IPreprocessorExpression second)
@@ -15,18 +15,43 @@ public class BinaryExpression(
     public string EvaluateExpression(IMacroContext context)
     {
         var firstValue = First.EvaluateExpression(context);
+        var parsedFirstValue = Parse(firstValue ?? throw new Exception());
+
         var secondValue = Second.EvaluateExpression(context);
-        return Operator switch
+        var parsedSecondValue = Parse(secondValue ?? throw new Exception());
+
+        var result = Operator switch
         {
-            CPreprocessorOperator.Equals => firstValue == secondValue ? "1" : "0",
-            CPreprocessorOperator.NotEquals => firstValue != secondValue ? "1" : "0",
-            CPreprocessorOperator.LessOrEqual => (firstValue ?? "").CompareTo(secondValue ?? "") <= 0 ? "1" : "0",
-            CPreprocessorOperator.GreaterOrEqual => (firstValue ?? "").CompareTo(secondValue ?? "") >= 0 ? "1" : "0",
-            CPreprocessorOperator.LessThan => (firstValue ?? "").CompareTo(secondValue ?? "") < 0 ? "1" : "0",
-            CPreprocessorOperator.GreaterThan => (firstValue ?? "").CompareTo(secondValue ?? "") > 0 ? "1" : "0",
-            CPreprocessorOperator.LogicalAnd => firstValue.AsBoolean() && secondValue.AsBoolean() ? "1" : "0",
-            CPreprocessorOperator.LogicalOr => firstValue.AsBoolean() || secondValue.AsBoolean() ? "1" : "0",
+            CPreprocessorOperator.Equals => parsedFirstValue == parsedSecondValue,
+            CPreprocessorOperator.NotEquals => parsedFirstValue != parsedSecondValue,
+            CPreprocessorOperator.LessOrEqual => parsedFirstValue <= parsedSecondValue,
+            CPreprocessorOperator.GreaterOrEqual => parsedFirstValue >= parsedSecondValue,
+            CPreprocessorOperator.LessThan => parsedFirstValue < parsedSecondValue,
+            CPreprocessorOperator.GreaterThan => parsedFirstValue > parsedSecondValue,
+            CPreprocessorOperator.LogicalAnd => parsedFirstValue.AsBoolean() && parsedSecondValue.AsBoolean(),
+            CPreprocessorOperator.LogicalOr => parsedFirstValue.AsBoolean() || parsedSecondValue.AsBoolean(),
             _ => throw new CompilationException($"Operator {Operator} cannot be used in the preprocessor directives"),
         };
+
+        return BooleanToString(result);
+
+        string BooleanToString(bool expressionResult) => expressionResult ? "1" : "0";
+
+        int Parse(string macrosValue)
+        {
+            if (int.TryParse(macrosValue, out var parsedMacroValue))
+                return parsedMacroValue;
+
+            if (macrosValue.StartsWith("0b"))
+                return Convert.ToInt32(macrosValue[2..], 2);
+
+            if (macrosValue.StartsWith("0x"))
+                return Convert.ToInt32(macrosValue[2..], 16);
+
+            if (macrosValue[0] == '0' && char.IsDigit(macrosValue[1]))
+                return Convert.ToInt32(macrosValue[2..], 8);
+
+            throw new NotSupportedException("Invalid macros format");
+        }
     }
 }
