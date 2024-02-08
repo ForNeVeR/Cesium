30c1cdfdec43ccb961c17d235ebb70268a2c91b6
Andrii Kurdiumov <kant2002@gmail.com>
2024-02-08 19:26:48 +0600

    Add _Bool type

--- Cesium.CodeGen/Ir/Declarations/LocalDeclarationInfo.cs
+++ Cesium.CodeGen/Ir/Declarations/LocalDeclarationInfo.cs
@@ -406,6 +406,7 @@ internal sealed record LocalDeclarationInfo(
                 ["double"] => PrimitiveTypeKind.Double,
                 ["__nint"] => PrimitiveTypeKind.NativeInt,
                 ["__nuint"] => PrimitiveTypeKind.NativeUInt,
+                ["_Bool"] => PrimitiveTypeKind.UnsignedChar,
                 _ => throw new WipException(
                     224,
                     $"Simple type specifiers are not supported: {string.Join(" ", typeNames)}"),
