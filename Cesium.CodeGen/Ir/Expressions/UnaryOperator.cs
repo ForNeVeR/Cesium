a44ea1f6ea7d33c95228b57561ec8328868adf5e
Andrey <54708336+BadRyuner@users.noreply.github.com>
2024-03-23 12:55:34 +0300

    (#197) Add unary plus

--- Cesium.CodeGen/Ir/Expressions/UnaryOperator.cs
+++ Cesium.CodeGen/Ir/Expressions/UnaryOperator.cs
@@ -3,6 +3,7 @@ namespace Cesium.CodeGen.Ir.Expressions;
 public enum UnaryOperator
 {
     Negation, // -
+    Promotion, // +
     BitwiseNot, // ~
     LogicalNot, // !
     AddressOf, // &
--- Cesium.CodeGen/Ir/Expressions/UnaryOperatorExpression.cs
+++ Cesium.CodeGen/Ir/Expressions/UnaryOperatorExpression.cs
@@ -55,6 +55,11 @@ internal sealed class UnaryOperatorExpression : IExpression
                 scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4_0));
                 scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ceq));
                 break;
+            case UnaryOperator.Promotion:
+                Target.EmitTo(scope);
+                if (ShouldBePromoted(Target.GetExpressionType((IDeclarationScope)scope)))
+                    scope.Method.Body.Instructions.Add(Instruction.Create(OpCodes.Conv_I4));
+                break;
             default:
                 Target.EmitTo(scope);
                 scope.Method.Body.Instructions.Add(GetInstruction());
@@ -72,12 +77,17 @@ internal sealed class UnaryOperatorExpression : IExpression
     public IType GetExpressionType(IDeclarationScope scope) => Operator switch
     {
         UnaryOperator.AddressOf => Target.GetExpressionType(scope).MakePointerType(), // address-of returns T*
+        UnaryOperator.Promotion => ShouldBePromoted(Target.GetExpressionType(scope)) ? new PrimitiveType(PrimitiveTypeKind.Int) : Target.GetExpressionType(scope),
         _ => Target.GetExpressionType(scope), // other operators return T
     };
+
+    // TargetArchitectureSet is not important. Short way to check for int, short, byte.
+    private static bool ShouldBePromoted(IType input) => input is PrimitiveType pt && (pt.GetSizeInBytes(TargetArchitectureSet.Bit32)! < 4);
 
     private static UnaryOperator GetOperatorKind(string @operator) => @operator switch
     {
         "-" => UnaryOperator.Negation,
+        "+" => UnaryOperator.Promotion,
         "!" => UnaryOperator.LogicalNot,
         "~" => UnaryOperator.BitwiseNot,
         "&" => UnaryOperator.AddressOf,
