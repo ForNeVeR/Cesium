b8279d004996dcc1648e9f438ac2d7bc4d446f52
Andrii Kurdiumov <kant2002@gmail.com>
2024-09-20 14:51:19 +0500

    Allow const eval expression in array declarations

--- Cesium.CodeGen/ConstantEvaluator.cs
+++ Cesium.CodeGen/ConstantEvaluator.cs
@@ -2,6 +2,7 @@ using Cesium.CodeGen.Ir.Expressions;
 using Cesium.CodeGen.Ir.Expressions.BinaryOperators;
 using Cesium.CodeGen.Ir.Expressions.Constants;
 using Cesium.Core;
+using System.Diagnostics;
 
 namespace Cesium.CodeGen;
 
@@ -9,64 +10,76 @@ internal static class ConstantEvaluator
 {
     public static IConstant GetConstantValue(IExpression expression)
     {
+        var result = TryGetConstantValue(expression);
+        if (result.ErrorMessage is not null)
+        {
+            throw new CompilationException(result.ErrorMessage);
+        }
+
+        Debug.Assert(result.Constant != null);
+        return result.Constant;
+    }
+
+    public static (string? ErrorMessage, IConstant? Constant) TryGetConstantValue(IExpression expression)
+    {
         switch (expression)
         {
             case ConstantLiteralExpression literal:
-                return literal.Constant;
+                return (null, literal.Constant);
 
             case UnaryOperatorExpression unOp:
-            {
-                var constant = GetConstantValue(unOp.Target);
+                {
+                    var constant = GetConstantValue(unOp.Target);
 
-                if (constant is not IntegerConstant constInt)
-                    throw new CompilationException("Evaluated constant is not an integer");
+                    if (constant is not IntegerConstant constInt)
+                        return ("Evaluated constant is not an integer", null);
 
                     return unOp.Operator switch
                     {
-                        UnaryOperator.Negation => new IntegerConstant(-constInt.Value),
-                        UnaryOperator.BitwiseNot => new IntegerConstant(~constInt.Value),
-                        UnaryOperator.LogicalNot => new IntegerConstant(constInt.Value != 0 ? 0 : 1),
-                        UnaryOperator.AddressOf or UnaryOperator.Indirection => throw new CompilationException($"Operator {unOp.Operator} is not compile-time evaluable"),
-                        _ => throw new ArgumentOutOfRangeException($"Invalid unary operator {unOp.Operator}"),
+                        UnaryOperator.Negation => (null, new IntegerConstant(-constInt.Value)),
+                        UnaryOperator.BitwiseNot => (null, new IntegerConstant(~constInt.Value)),
+                        UnaryOperator.LogicalNot => (null, new IntegerConstant(constInt.Value != 0 ? 0 : 1)),
+                        UnaryOperator.AddressOf or UnaryOperator.Indirection => ($"Operator {unOp.Operator} is not compile-time evaluable", null),
+                        _ => ($"Invalid unary operator {unOp.Operator}", null),
                     };
                 }
 
             case BinaryOperatorExpression binOp:
-            {
-                var leftConstant = GetConstantValue(binOp.Left);
-                var rightConstant = GetConstantValue(binOp.Right);
+                {
+                    var leftConstant = GetConstantValue(binOp.Left);
+                    var rightConstant = GetConstantValue(binOp.Right);
 
-                if (leftConstant is not IntegerConstant leftInt ||
-                    rightConstant is not IntegerConstant rightInt)
-                    throw new CompilationException("Evaluated constants are not integer");
+                    if (leftConstant is not IntegerConstant leftInt ||
+                        rightConstant is not IntegerConstant rightInt)
+                        throw new CompilationException("Evaluated constants are not integer");
 
-                return binOp.Operator switch
+                    return binOp.Operator switch
                     {
-                        BinaryOperator.Add => new IntegerConstant(leftInt.Value + rightInt.Value),
-                        BinaryOperator.Subtract => new IntegerConstant(leftInt.Value - rightInt.Value),
-                        BinaryOperator.Multiply => new IntegerConstant(leftInt.Value * rightInt.Value),
-                        BinaryOperator.Divide => new IntegerConstant(leftInt.Value / rightInt.Value),
-                        BinaryOperator.Remainder => new IntegerConstant(leftInt.Value % rightInt.Value),
-                        BinaryOperator.BitwiseLeftShift => new IntegerConstant(leftInt.Value << (int)rightInt.Value),
-                        BinaryOperator.BitwiseRightShift => new IntegerConstant(leftInt.Value >> (int)rightInt.Value),
-                        BinaryOperator.BitwiseOr => new IntegerConstant(leftInt.Value | rightInt.Value),
-                        BinaryOperator.BitwiseAnd => new IntegerConstant(leftInt.Value & rightInt.Value),
-                        BinaryOperator.BitwiseXor => new IntegerConstant(leftInt.Value ^ rightInt.Value),
+                        BinaryOperator.Add => (null, new IntegerConstant(leftInt.Value + rightInt.Value)),
+                        BinaryOperator.Subtract => (null, new IntegerConstant(leftInt.Value - rightInt.Value)),
+                        BinaryOperator.Multiply => (null, new IntegerConstant(leftInt.Value * rightInt.Value)),
+                        BinaryOperator.Divide => (null, new IntegerConstant(leftInt.Value / rightInt.Value)),
+                        BinaryOperator.Remainder => (null, new IntegerConstant(leftInt.Value % rightInt.Value)),
+                        BinaryOperator.BitwiseLeftShift => (null, new IntegerConstant(leftInt.Value << (int)rightInt.Value)),
+                        BinaryOperator.BitwiseRightShift => (null, new IntegerConstant(leftInt.Value >> (int)rightInt.Value)),
+                        BinaryOperator.BitwiseOr => (null, new IntegerConstant(leftInt.Value | rightInt.Value)),
+                        BinaryOperator.BitwiseAnd => (null, new IntegerConstant(leftInt.Value & rightInt.Value)),
+                        BinaryOperator.BitwiseXor => (null, new IntegerConstant(leftInt.Value ^ rightInt.Value)),
                         // boolean constants are needed here
-                        BinaryOperator.GreaterThan => new IntegerConstant(leftInt.Value > rightInt.Value ? 1 : 0),
-                        BinaryOperator.LessThan => new IntegerConstant(leftInt.Value < rightInt.Value ? 1 : 0),
-                        BinaryOperator.GreaterThanOrEqualTo => new IntegerConstant(leftInt.Value >= rightInt.Value ? 1 : 0),
-                        BinaryOperator.LessThanOrEqualTo => new IntegerConstant(leftInt.Value <= rightInt.Value ? 1 : 0),
-                        BinaryOperator.EqualTo => new IntegerConstant(leftInt.Value == rightInt.Value ? 1 : 0),
-                        BinaryOperator.NotEqualTo => new IntegerConstant(leftInt.Value != rightInt.Value ? 1 : 0),
-                        BinaryOperator.LogicalAnd => new IntegerConstant((leftInt.Value != 0) && (rightInt.Value != 0) ? 1 : 0),
-                        BinaryOperator.LogicalOr => new IntegerConstant((leftInt.Value != 0) || (rightInt.Value != 0) ? 1 : 0),
+                        BinaryOperator.GreaterThan => (null, new IntegerConstant(leftInt.Value > rightInt.Value ? 1 : 0)),
+                        BinaryOperator.LessThan => (null, new IntegerConstant(leftInt.Value < rightInt.Value ? 1 : 0)),
+                        BinaryOperator.GreaterThanOrEqualTo => (null, new IntegerConstant(leftInt.Value >= rightInt.Value ? 1 : 0)),
+                        BinaryOperator.LessThanOrEqualTo => (null, new IntegerConstant(leftInt.Value <= rightInt.Value ? 1 : 0)),
+                        BinaryOperator.EqualTo => (null, new IntegerConstant(leftInt.Value == rightInt.Value ? 1 : 0)),
+                        BinaryOperator.NotEqualTo => (null, new IntegerConstant(leftInt.Value != rightInt.Value ? 1 : 0)),
+                        BinaryOperator.LogicalAnd => (null, new IntegerConstant((leftInt.Value != 0) && (rightInt.Value != 0) ? 1 : 0)),
+                        BinaryOperator.LogicalOr => (null, new IntegerConstant((leftInt.Value != 0) || (rightInt.Value != 0) ? 1 : 0)),
                         _ => throw new ArgumentOutOfRangeException($"Invalid binary operator {binOp.Operator}"),
                     };
                 }
 
             default:
-                throw new CompilationException($"Expression {expression} cannot be evaluated as constant expression.");
+                return ($"Expression {expression} cannot be evaluated as constant expression.", null);
         }
     }
 }
