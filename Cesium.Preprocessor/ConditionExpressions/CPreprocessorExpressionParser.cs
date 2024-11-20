867f270412d652363c9a93ae6f6ad21626f1c1e6
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-20 22:32:47 +0500

    Fix stack overflow in the preprocessor on complex expressions

--- Cesium.Preprocessor/ConditionExpressions/CPreprocessorExpressionParser.cs
+++ Cesium.Preprocessor/ConditionExpressions/CPreprocessorExpressionParser.cs
@@ -27,32 +27,57 @@ internal partial class CPreprocessorExpressionParser
     [Rule("simple_expression: identifier")]
     private static IPreprocessorExpression MakeSimpleExpression(IPreprocessorExpression expression) => expression;
 
-    [Rule("binary_expression: expression '==' expression")]
-    [Rule("binary_expression: expression '!=' expression")]
-    [Rule("binary_expression: expression '<=' expression")]
-    [Rule("binary_expression: expression '>=' expression")]
-    [Rule("binary_expression: expression '<' expression")]
-    [Rule("binary_expression: expression '>' expression")]
-    [Rule("binary_expression: expression '+' expression")]
-    [Rule("binary_expression: expression '-' expression")]
-    [Rule("binary_expression: expression '*' expression")]
-    [Rule("binary_expression: expression '/' expression")]
-    [Rule("binary_expression: expression '||' expression")]
-    [Rule("binary_expression: expression '&&' expression")]
-    private static BinaryExpression MakeBinaryExpression(IPreprocessorExpression left, ICPreprocessorToken token, IPreprocessorExpression right)
-        => new(left, GetOperator(token), right);
+    [Rule("multiplicative_expression: multiplicative_expression '*' cast_expression")]
+    [Rule("multiplicative_expression: multiplicative_expression '/' cast_expression")]
+    [Rule("multiplicative_expression: multiplicative_expression '%' cast_expression")]
+    [Rule("additive_expression: additive_expression '+' multiplicative_expression")]
+    [Rule("additive_expression: additive_expression '-' multiplicative_expression")]
+    [Rule("shift_expression: shift_expression '<=' additive_expression")]
+    [Rule("shift_expression: shift_expression '>=' additive_expression")]
+    [Rule("relational_expression: relational_expression '<=' shift_expression")]
+    [Rule("relational_expression: relational_expression '>=' shift_expression")]
+    [Rule("relational_expression: relational_expression '<' shift_expression")]
+    [Rule("relational_expression: relational_expression '>' shift_expression")]
+    [Rule("equality_expression: equality_expression '==' relational_expression")]
+    [Rule("equality_expression: equality_expression '!=' relational_expression")]
+    [Rule("and_expression: and_expression '&' equality_expression")]
+    [Rule("exclusive_or_expression: exclusive_or_expression '^' and_expression")]
+    [Rule("inclusive_or_expression: inclusive_or_expression '|' exclusive_or_expression")]
+    [Rule("logical_or_expression: logical_or_expression '||' logical_and_expression")]
+    [Rule("logical_and_expression: logical_and_expression '&&' inclusive_or_expression")]
+    private static IPreprocessorExpression MakeBinaryExpression(IPreprocessorExpression left, ICPreprocessorToken token, IPreprocessorExpression right)
+        => new BinaryExpression(left, GetOperator(token), right);
 
-    [Rule("prefix_expression: '!' expression")]
-    private static UnaryExpression MakePrefixExpression(ICPreprocessorToken token, IPreprocessorExpression expression)
-        => new(token.Location, GetOperator(token), expression);
+    [Rule("conditional_expression: logical_or_expression '?' expression ':' conditional_expression")]
+    private static IPreprocessorExpression MakeConditionalExpression(IPreprocessorExpression condition, ICPreprocessorToken token, IPreprocessorExpression trueExpression, ICPreprocessorToken token2, IPreprocessorExpression falseExpression)
+        => new ConditionalExpression(condition, trueExpression, falseExpression);
 
-    [Rule("expression: identifier")]
-    [Rule("expression: binary_expression")]
-    [Rule("expression: prefix_expression")]
-    [Rule("expression: identifier_defined")]
+    [Rule("unary_expression: '!' cast_expression")]
+    [Rule("unary_expression: '+' cast_expression")]
+    [Rule("unary_expression: '-' cast_expression")]
+    [Rule("unary_expression: '~' cast_expression")]
+    private static IPreprocessorExpression MakeUnaryExpression(ICPreprocessorToken token, IPreprocessorExpression expression)
+        => new UnaryExpression(token.Location, GetOperator(token), expression);
+
+    [Rule("postfix_expression: identifier")]
+    [Rule("unary_expression: identifier_defined")]
+    [Rule("unary_expression: postfix_expression")]
+    [Rule("cast_expression: unary_expression")]
+    [Rule("multiplicative_expression: cast_expression")]
+    [Rule("additive_expression: multiplicative_expression")]
+    [Rule("shift_expression: additive_expression")]
+    [Rule("relational_expression: shift_expression")]
+    [Rule("equality_expression: relational_expression")]
+    [Rule("and_expression: equality_expression")]
+    [Rule("exclusive_or_expression: and_expression")]
+    [Rule("inclusive_or_expression: exclusive_or_expression")]
+    [Rule("logical_and_expression: inclusive_or_expression")]
+    [Rule("logical_or_expression: logical_and_expression")]
+    [Rule("conditional_expression: logical_or_expression")]
+    [Rule("expression: conditional_expression")]
     private static IPreprocessorExpression MakeExpression(IPreprocessorExpression expression) => expression;
 
-    [Rule("expression: '(' expression ')' ")]
+    [Rule("postfix_expression: '(' conditional_expression ')' ")]
     private static IPreprocessorExpression MakeExpression(ICPreprocessorToken lparen, IPreprocessorExpression expression, ICPreprocessorToken rparen) => expression;
 
     private static CPreprocessorOperator GetOperator(ICPreprocessorToken token) => token.Text switch
