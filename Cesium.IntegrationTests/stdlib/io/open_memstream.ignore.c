6c758f2299556b76322b71e79c14970215b7e289
Andrii Kurdiumov <kant2002@gmail.com>
2025-10-10 23:21:15 +0500

    Experimental open_memsrteam extension
    https://www.cppreference.com/w/c/experimental/dynamic.html

---
+++ Cesium.IntegrationTests/stdlib/io/open_memstream.ignore.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    FILE* stream;
+    char* buffer;
+    size_t size;
+
+    // Open a memory stream
+    stream = open_memstream(&buffer, &size);
+    if (stream == NULL) {
+        perror("open_memstream");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write data to the stream
+    fprintf(stream, "Hello, ");
+    fprintf(stream, "world!");
+
+    // Flush the stream to update the buffer and size
+    fflush(stream);
+    printf("Buffer: %s\n", buffer);
+    printf("Size: %zu\n", size);
+
+    // Close the stream and free the buffer
+    fclose(stream);
+    free(buffer);
+
+    return 42;
+}
