e8d1df2ea6cdd1c5854c6b1de961163da6d8776f
Andrii Kurdiumov <kant2002@gmail.com>
2025-10-11 08:53:04 +0500

    Add strstr to runtime library - Added unit tests - Added integration tests - Rename test class so StringTests would hold tests for string.h

--- Cesium.Runtime.Tests/StringTests.cs
+++ Cesium.Runtime.Tests/StringTests.cs
@@ -8,25 +8,22 @@ namespace Cesium.Runtime.Tests;
 public unsafe class StringTests
 {
     [Fact]
-    public void ComplexTest()
+    public void StrSrtTest()
     {
         Assert.Equal(sizeof(long), sizeof(UTF8String));
 
-        UTF8String someString = (byte*)Marshal.StringToHGlobalAnsi("Hello world!\0");
+        UTF8String someString = (byte*)Marshal.StringToHGlobalAnsi("one two three!\0");
+        Assert.Equal(4L, LocalTest("two"));
+        Assert.Equal(0L, LocalTest(""));
+        Assert.Equal(-1, LocalTest("nine"));
+        Assert.Equal(1, LocalTest("n"));
 
-        Assert.Equal("Hello world!".Length, (int)someString.Length);
-        Assert.Equal("Hello world!".Length + 1, (int)someString.NullTerminatedLength);
-        Assert.Equal("Hello world!", someString.ToString());
-
-        UTF8String someMemory = stackalloc byte[(int)someString.NullTerminatedLength];
-        someString.CopyTo(someMemory);
-        Assert.Equal(someString.ToString(), someMemory.ToString());
-
-        UTF8String someOtherMemory = stackalloc byte[(int)someString.NullTerminatedLength];
-        someString.CopyTo(someOtherMemory, 5);
-        someOtherMemory[6] = (byte)'\0';
-        Assert.Equal("Hello", someOtherMemory.ToString());
-
-        Assert.Equal((byte)'o', someString.At(4)[0]);
+        long LocalTest(string text)
+        {
+            UTF8String substr = (byte*)Marshal.StringToHGlobalAnsi($"{text}\0");
+            var result = StringFunctions.StrStr(someString, substr);
+            if (!result) return -1;
+            return (byte*)result - (byte*)someString;
+        }
     }
 }
