382c2652746fde7b6423775384b51e3e7065235d
Nikita Turnitsky <96059564+nt-devilboi@users.noreply.github.com>
2025-11-17 02:52:45 +0500

    532 support negative numbers (#872)
    * add recognize negative number
    
    * add plus
    
    * add tests and plus in UnaryExpression.cs
    
    ---------
    
    Co-authored-by: devilboi95 <turnickii.n@gmail>

--- Cesium.Parser.Tests/PreprocessorTests/PreprocessorTests.cs
+++ Cesium.Parser.Tests/PreprocessorTests/PreprocessorTests.cs
@@ -909,13 +909,22 @@ char* x = __FILE__;
     [InlineData("1 || 0", true)]
     [InlineData("1 || 1", true)]
 
-    // TODO[#532]: Need to add support for parsing negative numbers, now "-" is recognized as a separator
-    // [InlineData("-10 < 9", true)]
-    // [InlineData("-10 > 9", false)]
 
     [InlineData("0b11 == 3", true)]
     [InlineData("021 == 17", true)]
     [InlineData("0xF == 15", true)]
+
+
+    [InlineData("-10 > 9", false)]
+    [InlineData("-10 < 9", true)]
+    [InlineData("-10 < -9", true)]
+    [InlineData("-9 == -9", true)]
+    [InlineData("-9 <= -9", true)]
+    [InlineData("-9 >= 10", false)]
+    [InlineData(" -10 < 0x10", true)]
+    [InlineData(" -0 == 0", true)]
+    [InlineData(" -0 == +0", true)]
+    [InlineData(" -10 != +10", true)]
     public async Task EvaluateExpressionAllVariants(
         string expression,
         bool expectedResult)
