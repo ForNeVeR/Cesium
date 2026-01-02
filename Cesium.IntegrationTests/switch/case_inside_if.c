4b548a90daf049bb526d53503397bbef85524ba7
Andrii Kurdiumov <kant2002@gmail.com>
2026-01-02 14:27:13 +0100

    Add test for using case statement inside if statement.

---
+++ Cesium.IntegrationTests/switch/case_inside_if.c
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Cesium contributors <https://github.com/ForNeVeR/Cesium>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+int main()
+{
+    int j = 42;
+    int c = 3;
+    bool altered = false;
+
+    switch(code) {
+        case 3:
+            if (altered) {
+        case 2:
+                j++;
+            }
+            break;
+    }
+
+    return j;
+}
