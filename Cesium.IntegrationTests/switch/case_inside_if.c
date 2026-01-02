8d30c52a917314aa6775f02edad384330591a3ff
Andrii Kurdiumov <kant2002@gmail.com>
2026-01-02 16:34:31 +0100

    Fixed case
    In this case additional variables generated

--- Cesium.IntegrationTests/switch/case_inside_if.c
+++ Cesium.IntegrationTests/switch/case_inside_if.c
@@ -6,17 +6,27 @@
 
 int main()
 {
-    int j = 42;
+    int j = 41;
     int c = 3;
     int altered = 0;
 
-    switch(code) {
+    switch(c) {
         case 3:
             if (altered) {
         case 2:
                 j++;
             }
             break;
+    }
+
+    c = 2;
+    switch (c) {
+    case 3:
+        if (altered) {
+    case 2:
+        j++;
+        }
+        break;
     }
 
     return j;
