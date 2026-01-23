cc3472ac0385ec699786e3beca1fb77c8e97d243
Andrii Kurdiumov <kant2002@gmail.com>
2026-01-24 02:50:12 +0500

    Add test for using case statement inside if statement. (#919)

--- Cesium.CodeGen.Tests/CodeGenSwitchTests.cs
+++ Cesium.CodeGen.Tests/CodeGenSwitchTests.cs
@@ -108,6 +108,35 @@ public class CodeGenSwitchTests : CodeGenTestBase
 }");
 
     [Fact]
+    public Task DeepCase2() => DoTest(@"int main()
+{
+    int j = 41;
+    int c = 3;
+    int altered = 0;
+
+    switch(c) {
+        case 3:
+            if (altered) {
+        case 2:
+                j++;
+            }
+            break;
+    }
+
+    c = 2;
+    switch (c) {
+    case 3:
+        if (altered) {
+    case 2:
+        j++;
+        }
+        break;
+    }
+
+    return j;
+}");
+
+    [Fact]
     public Task LowerExpression() => DoTest(@"
 int my_condition() { return 0; }
 int main()
