816b5a34dab9da1d265db7d94d6e60e030d97812
Andrii Kurdiumov <kant2002@gmail.com>
2026-02-14 17:33:10 +0100

    1

--- Cesium.IntegrationTests/structs/structs_unions.nonportable.c
+++ Cesium.IntegrationTests/structs/structs_unions.nonportable.c
@@ -32,11 +32,15 @@ int test() {
 }
 
 int main() {
+    if (test() != 10) {
+        return -1;
+    }
+
     foo f;
     f._1 = 2;
     f._2a = 10;
     f._3u = 10;
     f.uni._5u = 10;
     f.s._7 = 10;
-    return f._1 + f._2a + f._4u + f.uni._6u + f.s._7 + (test() - 10);
+    return f._1 + f._2a + f._4u + f.uni._6u + f.s._7;
 }
