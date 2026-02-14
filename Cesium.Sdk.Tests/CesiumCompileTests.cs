6c19a5e17563b36e13b107a0c2ace1644f9ab90f
Andrii Kurdiumov <kant2002@gmail.com>
2026-02-14 12:24:10 +0100

    3

--- Cesium.Sdk.Tests/CesiumCompileTests.cs
+++ Cesium.Sdk.Tests/CesiumCompileTests.cs
@@ -80,8 +80,9 @@ public class CesiumCompileTests(ITestOutputHelper testOutputHelper) : SdkTestBas
         var result = await ExecuteTargets(projectName, "Restore", "Build");
 
         Assert.Equal(1, result.ExitCode);
-        Assert.Equal("Unsupported TargetFramework: net461. Supported frameworks are: net6.0 and up, netstandard2.0 and net462 and up.", result.StdErrOutput);
-        Assert.Empty(result.IntermediateArtifacts);
+        Assert.Equal(
+            "Unsupported TargetFramework: net461. Supported frameworks are: net6.0 and up, netstandard2.0 and net462 and up.",
+            result.StdOutOutput);
         Assert.Empty(result.OutputArtifacts);
     }
 
